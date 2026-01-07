4b62cba42e7a117dceb723505f39846d075c1a4d
Gabi Bostan <gabibostan@MAC-16450D.station>
2026-01-07 20:44:43 +0000

    Format modification

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -37,16 +37,16 @@ module "network" {
       address_prefixes = ["10.0.1.0/24"]
     }
     snet-container-apps = {
-  address_prefixes = ["10.0.3.0/26"]
+      address_prefixes = ["10.0.3.0/26"]
 
-  delegation = {
-    name = "delegation-container-apps"
-    service_delegation = {
-      name    = "Microsoft.App/environments"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+      delegation = {
+        name = "delegation-container-apps"
+        service_delegation = {
+          name    = "Microsoft.App/environments"
+          actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+        }
+      }
     }
-  }
-}
   }
 
   tags = local.tags
